7ab170af73a98559ec1ff279d2df0d9654e2a84e
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-12-20 19:43:47 -0700

    template updates

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
- # aslkfs;
-    ## Description
+ # aslkfs
+## Description
     sl;akfa
-    ## Table of Contents
-    * [Usage](#usage)
-    * [Installation](#installation)
-    * [Contributors](#contributors)
-    * [Tests](#tests)
-    * [Licenses](#licenses)
-    * [Contact](#contact)
-    ## Usage
-    slakfjs;al
-    ## Installation
-    slkafjas;l
-    ## Contributors
-    slkjfa;
-    ## Tests
-    undefined
-    ## Licenses
-    MIT
-    ## Contact
-    slkafs;al
-    
+## Table of Contents
+* [Usage](#usage)
+* [Installation](#installation)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Licenses](#licenses)
+* [Contact](#contact)
+## Usage
+slakfjs;al
+## Installation
+slkafjas;l
+## Contributors
+slkjfa;
+## Tests
+undefined
+## Licenses
+MIT
+## Contact
+lkafs;al
--- src/page-template.js
+++ src/page-template.js
@@ -1,27 +1,27 @@
 const generateMarkDown = (data) => {
-    return ` # ${data.title}
-    ## Description
-    ${data.description}
-    ## Table of Contents
-    * [Usage](#usage)
-    * [Installation](#installation)
-    * [Contributors](#contributors)
-    * [Tests](#tests)
-    * [Licenses](#licenses)
-    * [Contact](#contact)
-    ## Usage
-    ${data.usage}
-    ## Installation
-    ${data.installation}
-    ## Contributors
-    ${data.contributors}
-    ## Tests
-    ${data.tests}
-    ## Licenses
-    ${data.licenses}
-    ## Contact
-    ${data.email}
-    `
+return ` # ${data.title}
+## Description
+${data.description}
+## Table of Contents
+* [Usage](#usage)
+* [Installation](#installation)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Licenses](#licenses)
+* [Contact](#contact)
+## Usage
+${data.usage}
+## Installation
+${data.installation}
+## Contributors
+${data.contributors}
+## Tests
+${data.tests}
+## Licenses
+${data.licenses}
+## Contact
+${data.email}
+`
 }
 
 module.exports = generateMarkDown
